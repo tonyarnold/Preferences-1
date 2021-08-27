094f389d9838f07d05dda992d70494c7a34741fb
Tony Arnold <tony@thecocoabots.com>
2021-08-27 14:48:43 +1000

    Allow initializing with a custom windowFrameAutosaveName
    This prevents transient entries of "com.sindresorhus.Preferences.FrameAutosaveName" in the app's defaults.

--- Sources/Preferences/PreferencesWindowController.swift
+++ Sources/Preferences/PreferencesWindowController.swift
@@ -29,7 +29,8 @@ public final class PreferencesWindowController: NSWindowController {
 		preferencePanes: [PreferencePane],
 		style: Preferences.Style = .toolbarItems,
 		animated: Bool = true,
-		hidesToolbarForSingleItem: Bool = true
+		hidesToolbarForSingleItem: Bool = true,
+		windowFrameAutosaveName: NSWindow.FrameAutosaveName? = nil
 	) {
 		precondition(!preferencePanes.isEmpty, "You need to set at least one view controller")
 
@@ -49,7 +50,7 @@ public final class PreferencesWindowController: NSWindowController {
 		self.hidesToolbarForSingleItem = hidesToolbarForSingleItem
 		super.init(window: window)
 
-		self.windowFrameAutosaveName = .preferences
+		self.windowFrameAutosaveName = windowFrameAutosaveName ?? .preferences
 
 		window.contentViewController = tabViewController
 
